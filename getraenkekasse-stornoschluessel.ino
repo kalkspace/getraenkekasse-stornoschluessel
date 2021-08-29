70bc5c7dc8f4be43572d5ddc815cb8827690ffdd
Andreas Streichardt <andreas@mop.koeln>
2021-08-29 10:25:50 +0200

    ✨ use arduino nano instead of all the fancy stuff that just breaks somewhen

--- getraenkekasse-stornoschluessel.ino
+++ getraenkekasse-stornoschluessel.ino
@@ -1,4 +1,4 @@
-int stornoPin = 10;
+int stornoPin = 2;
 int value;
 int state;
 
@@ -7,14 +7,17 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(stornoPin, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   value = digitalRead(stornoPin);
   if (value != state) {
     if (value == LOW) {
+      digitalWrite(LED_BUILTIN, HIGH);
       Serial.write("storno\n");
     } else {
+      digitalWrite(LED_BUILTIN, LOW);
       Serial.write("stornoend\n");
     }
     
